91d76e484459ede7fce8829ca50ef11b214bbbfe
Rodrigo Eltz <rpetrye@hotmail.com>
2019-02-06 08:06:41 -0500

    First screen and buttons added

--- app/src/main/java/com/example/flooringmobile/MainActivity.java
+++ app/src/main/java/com/example/flooringmobile/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.flooringmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button boxes = findViewById(R.id.boxCalc);
+
+        boxes.setOnClickListener(b -> {
+            Intent goBoxes = new Intent(MainActivity.this, BoxCalc.class);
+            startActivity(goBoxes);
+        });
+
+
+
     }
+
+
+
+
 }
