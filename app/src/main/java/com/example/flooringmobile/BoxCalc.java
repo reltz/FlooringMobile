833ac80cf9c790ccb20609023b1f1873b96ea60a
Rodrigo Eltz <rpetrye@hotmail.com>
2019-02-07 10:09:15 -0500

    First screen and buttons added

--- app/src/main/java/com/example/flooringmobile/BoxCalc.java
+++ app/src/main/java/com/example/flooringmobile/BoxCalc.java
@@ -3,26 +3,29 @@ package com.example.flooringmobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class BoxCalc extends AppCompatActivity {
-    protected float measure;
+    protected double measure;
     protected EditText myMeasure;
     protected Button calculate;
     protected Button resetIt;
 
     protected EditText boxCoverage;
-    protected float coverage;
+    protected double coverage;
+    protected CheckBox ten;
 
     protected EditText price;
-    protected float thePrice;
+    protected double thePrice;
 
     protected TextView display;
 
     protected String output;
-    protected float boxNumber;
-    protected float priceSqFt;
+    protected double boxNumber;
+    protected double priceSqFt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +43,34 @@ public class BoxCalc extends AppCompatActivity {
         //price
         price = findViewById(R.id.price1);
 
+        //10percent
+        ten = findViewById(R.id.checkBox);
+
+        ten.setOnClickListener(t -> {
+            if (ten.isChecked()) {
+                measure = Float.valueOf(myMeasure.getText().toString())*1.1;
+            }
+            else {
+                measure = Float.valueOf(myMeasure.getText().toString());
+            }
+
+        });
+
+
 
 
         display = findViewById(R.id.outputBox);
         calculate.setOnClickListener( b-> {
             //exception handling needed -> boxes cant be empty!
+
+
                 thePrice = Float.valueOf(price.getText().toString());
                 coverage = Float.valueOf(boxCoverage.getText().toString());
-                measure = Float.valueOf(myMeasure.getText().toString());
-                boxNumber = (measure / coverage);
+                //measure = Float.valueOf(myMeasure.getText().toString());
+
+
+                    boxNumber = (measure  / coverage);
+
                 output = "You need " + boxNumber + " boxes!";
                 display.setText(output);
 
